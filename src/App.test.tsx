b78ecb1cf9eddd128d35fcce54c4b17009577428
Laurent Sanchez <lsanchez.ext@galerieslafayette.com>
2018-04-09 14:06:19 +0200

    install enzyme adapter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  Enzyme.shallow(<App />);
 });
