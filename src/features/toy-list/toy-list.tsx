edba42a6ef38d995d71c84fe3f49685c840773ad
Laurent Sanchez <laurentsanchez@macbook-pro-de-laurent-2.home>
2018-04-19 00:11:22 +0200

    add AnimateHeight plugin

--- src/features/toy-list/toy-list.tsx
+++ src/features/toy-list/toy-list.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ToyComponent, Toy } from './toy';
 import './toy-list.css';
 import '../../../node_modules/@mdi/font/css/materialdesignicons.min.css';
+import AnimateHeight from 'react-animate-height';
 
 interface Props {
   toyList: Toy[];
@@ -10,32 +11,39 @@ interface Props {
 }
 
 interface State {
-  hidden: boolean;
+  height: number | string;
 }
 
 export class ToyListComponent extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
-    this.state = { hidden: false };
-  }
-
-  public content = () => this.props.toyList.map((toy: Toy) => {
-    return <ToyComponent key={toy.title} toy={toy} select={this.props.select} />;
-  })
-
-  public resize = () => {
-    return this.state.hidden ? ({ flexGrow: 0 }) : ({ flexGrow: 1 });
+    this.state = { height: 'auto' };
   }
 
   render() {
+    const { height } = this.state;
     return (
-      <main className="ToyListContainer">
-        <h1 style={{'paddingLeft': '1rem'}} onClick={() => this.setState({ hidden: !this.state.hidden})}>ToyList</h1>
-        <section className="ToyListComponent" style={this.resize()}>{this.content()}</section>
+      <main>
+        <h1 style={{'paddingLeft': '1rem'}} onClick={this.toggle}>ToyList</h1>
+        <AnimateHeight height={height} duration={200}>
+          <section className="ToyListComponent">{this.content()}</section>
+        </AnimateHeight>
         <section className="ToyList__unselect mdi mdi-close-circle" onClick={this.props.unselect}>Unselect All</section>
       </main>
     );
   }
+
+  private toggle = () => {
+    const { height } = this.state;
+
+    this.setState({
+      height: height === 0 ? 'auto' : 0
+    });
+  }
+
+  private content = () => this.props.toyList.map((toy: Toy) => {
+    return <ToyComponent key={toy.title} toy={toy} select={this.props.select} />;
+  })
 
 }
